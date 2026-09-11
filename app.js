9d9eae6b984b3a507eaa4b5928821faec320a16e
technoran <technoran@hotmail.com>
2026-09-10 21:56:57 -0300

    Update APPS_SCRIPT_URL to new endpoint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Control de pantallas
 let currentScreen = 1;
 let bookingBackScreen = 2;
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz7yRYxuAL7lanfdKRkLVS9h1KnJnOA_u5UbIy-1ltQR80XIluc-iadcnaiytnjlGvr8Q/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyKb7DcOX2i30cOvn6zqRNkI_hW_yEzJiMyFbb_MfUnp8cQa9BktAR2fRwASLSBiPFNOA/exec';
 
 function goToScreen(screenNumber) {
   document.getElementById(`screen${currentScreen}`).classList.remove('active');
